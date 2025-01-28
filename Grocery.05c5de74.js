a9c046b5c9cc28e477e410c6738b18ce57b75ce4
swarnimrajput <swarnimrajput5826@gmail.com>
2025-01-28 11:26:52 +0530

    Updates

---
+++ Grocery.05c5de74.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire94c2;(0,e.register)("loR3J",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var c=e("ayMG0"),l=()=>(0,c.jsxs)("h1",{children:["Welcome to our Grocery Store.",(0,c.jsx)("span",{children:"You can select The Groceries From Here."})]})});
+//# sourceMappingURL=Grocery.05c5de74.js.map
